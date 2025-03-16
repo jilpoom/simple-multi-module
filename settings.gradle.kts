161d7b6379d0f6cc329fddcb4a870ae796e8a9be
JeongJeongYong <wjddyd94@naver.com>
2025-03-16 20:16:46 +0900

    chore(board): add application, api modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,17 @@ include("core:jpa-core")
 
 findProject(":core:jpa-core")?.name = "jpa-core"
 findProject(":core:exception-handler-core")?.name = "exception-handler-core"
+
+include("services:board:api")
+findProject(":services:board:api")?.name = "api"
+
+include("services:board:api:domain")
+findProject(":services:board:api:domain")?.name = "domain"
+
+include("services:board:api:exception")
+findProject(":services:board:api:exception")?.name = "exception"
+
+include("services:board:api:readmodel")
+findProject(":services:board:api:readmodel")?.name = "readmodel"
+include("services:board:application")
+findProject(":services:board:application")?.name = "application"
